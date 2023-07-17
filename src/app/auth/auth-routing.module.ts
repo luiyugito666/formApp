bc2558033d22e6e7a4e217c8485528ccfb448ce2
jorge luis yucra <jorgeyh7@gmail.com>
2023-07-17 11:53:39 -0500

    rutas de la aplicacion

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'singn-up', component: RegisterPageComponent },
+      {path:'**',redirectTo:'singn-up'
+
+      }
+    ]
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
